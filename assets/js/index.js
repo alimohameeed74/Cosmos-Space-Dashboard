e32d34ba7d57937776e370f631ffd0b8b58ca387
alimohamed <aliimohamed2003@gmail.com>
2025-12-29 19:55:14 +0200

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,34 +2,53 @@
 // =========================================
 // API Documentation
 // https://tinyurl.com/cosmos-docs
+const apiKey='GODw1jxABAeKpmtcqmGfOg2JBmn63CmqhdiUqidT'
 // =========================================
 // =========================================
 // Sidebar Section
 // 1) Main Variables
-// 1) Main Functions
-// 1) Main Login
+// 2) Main Functions
+// 3) Main Login
 // =========================================
 // =========================================
 // Header Section
 // 1) Main Variables
-// 1) Main Functions
-// 1) Main Login
+// 2) Main Functions
+// 3) Main Login
 // =========================================
 // =========================================
 // Today-In-Space Section
 // 1) Main Variables
-// 1) Main Functions
-// 1) Main Login
+let pictureDate = document.getElementById("picture-date");
+let apodDateInput = document.getElementById("apod-date-input");
+let loadDateBtn = document.getElementById("load-date-btn");
+let todayApodBtn = document.getElementById("today-apod-btn");
+let apodImage = document.getElementById("apod-image");
+let apodImageFullHd = document.getElementById("apod-image-full-hd");
+let apodTitle = document.querySelector("#today-in-space #apod-title");
+let apodDateDetail = document.querySelector("#today-in-space #apod-date-detail");
+let apodExplanation = document.querySelector("#today-in-space #apod-explanation");
+let apodCopyright = document.querySelector("#today-in-space #apod-copyright");
+let apodDateInfo = document.querySelector("#today-in-space #apod-date-info");
+let apodMediaType = document.querySelector("#today-in-space #apod-media-type");
+// 2) Main Functions
+// 3) Main Login
+todayApodBtn.addEventListener('click', function(){
+    console.log("today");
+});
+loadDateBtn.addEventListener('click', function(){
+    console.log("laod");
+})
 // =========================================
 // =========================================
 // Launches Section
 // 1) Main Variables
-// 1) Main Functions
-// 1) Main Login
+// 2) Main Functions
+// 3) Main Login
 // =========================================
 // =========================================
 // Planets Section
 // 1) Main Variables
-// 1) Main Functions
-// 1) Main Login
+// 2) Main Functions
+// 3) Main Login
 // =========================================
