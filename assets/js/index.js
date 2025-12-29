57016259a5c430537481cdddb955a1ab81023818
alimohamed <aliimohamed2003@gmail.com>
2025-12-29 19:11:16 +0200

    inital commit

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+"use strict"
+// =========================================
+// API Documentation
+// https://tinyurl.com/cosmos-docs
+// =========================================
+// =========================================
+// Sidebar Section
+// 1) Main Variables
+// 1) Main Functions
+// 1) Main Login
+// =========================================
+// =========================================
+// Header Section
+// 1) Main Variables
+// 1) Main Functions
+// 1) Main Login
+// =========================================
+// =========================================
+// Today-In-Space Section
+// 1) Main Variables
+// 1) Main Functions
+// 1) Main Login
+// =========================================
+// =========================================
+// Launches Section
+// 1) Main Variables
+// 1) Main Functions
+// 1) Main Login
+// =========================================
+// =========================================
+// Planets Section
+// 1) Main Variables
+// 1) Main Functions
+// 1) Main Login
+// =========================================
